8dc4014572348242bf9e8fb43187d01d1a9e69a8
gabijerecelroa <gabijerecel@gmail.com>
2026-05-10 14:47:07 -0300

    Actualizar iconos CONEXION

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gjbross-v2';
+const CACHE = 'conexion-v3';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json', '/logo.jpg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
